83408a88196790ef21213a2e313388399326d1c4
Luke Childs <lukechilds123@gmail.com>
2016-05-22 18:50:51 +0100

    Add tests for checking returned DOM node

--- test/unit.js
+++ test/unit.js
@@ -4,3 +4,26 @@ import createNode from '../dist/create-node';
 test('createNode should be a function', t => {
   t.is(typeof createNode, 'function');
 });
+
+test('createNode(markup) should create an HTMLElement DOM node', t => {
+  const domNode = createNode('<div></div>');
+  t.true(domNode instanceof window.HTMLElement);
+});
+
+test('DOM node should match markup', t => {
+  const markup = `
+  <div>
+    <span>hello</span>
+    <span>world</span>
+  </div>
+  `;
+  const expectedText = ['hello', 'world'];
+  const domNode = createNode(markup);
+
+  t.true(domNode instanceof window.HTMLDivElement);
+  t.is(domNode.children.length, 2);
+  Array.from(domNode.children).forEach((childNode, i) => {
+    t.true(childNode instanceof window.HTMLSpanElement);
+    t.is(childNode.textContent, expectedText[i]);
+  });
+});
