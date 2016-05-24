d2f85a4abc6b0b78c75908d05b261a4fb9300b41
Luke Childs <lukechilds123@gmail.com>
2016-05-24 22:24:12 +0100

    Replace test helper with node-browser-environment

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,8 @@
 import test from 'ava';
+import browserEnv from 'node-browser-environment';
 import createNode from '../dist/create-node';
+
+browserEnv();
 
 test('createNode should be a function', t => {
   t.is(typeof createNode, 'function');
@@ -7,7 +10,7 @@ test('createNode should be a function', t => {
 
 test('createNode(markup) should create an HTMLElement DOM node', t => {
   const domNode = createNode('<div></div>');
-  t.true(domNode instanceof window.HTMLElement);
+  t.true(domNode instanceof HTMLElement);
 });
 
 test('DOM node should match markup', t => {
@@ -20,10 +23,10 @@ test('DOM node should match markup', t => {
   const expectedText = ['hello', 'world'];
   const domNode = createNode(markup);
 
-  t.true(domNode instanceof window.HTMLDivElement);
+  t.true(domNode instanceof HTMLDivElement);
   t.is(domNode.children.length, 2);
   Array.from(domNode.children).forEach((childNode, i) => {
-    t.true(childNode instanceof window.HTMLSpanElement);
+    t.true(childNode instanceof HTMLSpanElement);
     t.is(childNode.textContent, expectedText[i]);
   });
 });
