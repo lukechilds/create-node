934cfc0791d8e678d20d50cc05cc51d0fec184e9
Luke Childs <lukechilds123@gmail.com>
2016-11-11 06:42:54 +0000

    Return HTMLCollection if there are multiple top level elems

--- src/create-node.js
+++ src/create-node.js
@@ -1,5 +1,5 @@
 export default markup => {
   const container = document.createElement('div');
   container.innerHTML = markup;
-  return container.children[0];
+  return container.children.length > 1 ? container.children : container.children[0];
 }
