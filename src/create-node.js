304b86185d13796b417f1df0c3bdb7d3e40b25b3
Luke Childs <lukechilds123@gmail.com>
2017-03-11 17:25:29 +0700

    Keep XO happy

--- src/create-node.js
+++ src/create-node.js
@@ -1,5 +1,5 @@
 export default markup => {
-  const container = document.createElement('div');
-  container.innerHTML = markup;
-  return container.children.length > 1 ? container.children : container.children[0];
-}
+	const container = document.createElement('div');
+	container.innerHTML = markup;
+	return container.children.length > 1 ? container.children : container.children[0];
+};
--- test/unit.js
+++ test/unit.js
@@ -5,39 +5,39 @@ import createNode from '../dist/create-node';
 browserEnv();
 
 test('createNode should be a function', t => {
-  t.is(typeof createNode, 'function');
+	t.is(typeof createNode, 'function');
 });
 
 test('createNode(markup) with one top level element should create an HTMLElement DOM node', t => {
-  const domNode = createNode('<div></div>');
-  t.true(domNode instanceof HTMLElement);
+	const domNode = createNode('<div></div>');
+	t.true(domNode instanceof HTMLElement);
 });
 
 test('createNode(markup) with multiple top level elements should create an HTMLCollection of HTMLElement DOM nodes', t => {
-  const domNode = createNode('<div></div><div></div>');
-  t.true(domNode instanceof HTMLCollection);
+	const domNode = createNode('<div></div><div></div>');
+	t.true(domNode instanceof HTMLCollection);
 });
 
 test('DOM node should match markup', t => {
-  const markup = `
+	const markup = `
   <div>
     <span>hello</span>
     <span>world</span>
   </div>
   `;
-  const expectedText = ['hello', 'world'];
-  const domNode = createNode(markup);
+	const expectedText = ['hello', 'world'];
+	const domNode = createNode(markup);
 
-  t.true(domNode instanceof HTMLDivElement);
-  t.is(domNode.children.length, 2);
-  Array.from(domNode.children).forEach((childNode, i) => {
-    t.true(childNode instanceof HTMLSpanElement);
-    t.is(childNode.textContent, expectedText[i]);
-  });
+	t.true(domNode instanceof HTMLDivElement);
+	t.is(domNode.children.length, 2);
+	Array.from(domNode.children).forEach((childNode, i) => {
+		t.true(childNode instanceof HTMLSpanElement);
+		t.is(childNode.textContent, expectedText[i]);
+	});
 });
 
 test('DOM node should insert into DOM', t => {
-  const domNode = createNode('<div></div>');
-  document.body.appendChild(domNode)
-  t.is(document.body.lastChild, domNode);
+	const domNode = createNode('<div></div>');
+	document.body.appendChild(domNode);
+	t.is(document.body.lastChild, domNode);
 });
