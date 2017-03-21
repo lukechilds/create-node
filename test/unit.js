2d27f91c38eba2f6534d1dac31f0010f407111e0
Luke Childs <lukechilds123@gmail.com>
2017-03-21 11:57:04 +0800

    Replace browser-env with window

--- test/unit.js
+++ test/unit.js
@@ -1,24 +1,28 @@
 import test from 'ava';
-import browserEnv from 'browser-env';
+import Window from 'window';
 import createNode from '../dist/create-node';
-
-browserEnv();
 
 test('createNode should be a function', t => {
 	t.is(typeof createNode, 'function');
 });
 
 test('createNode(markup) with one top level element should create an HTMLElement DOM node', t => {
-	const domNode = createNode('<div></div>');
+	const { document, HTMLElement } = new Window();
+
+	const domNode = createNode('<div></div>', document);
 	t.true(domNode instanceof HTMLElement);
 });
 
 test('createNode(markup) with multiple top level elements should create an HTMLCollection of HTMLElement DOM nodes', t => {
-	const domNode = createNode('<div></div><div></div>');
+	const { document, HTMLCollection } = new Window();
+
+	const domNode = createNode('<div></div><div></div>', document);
 	t.true(domNode instanceof HTMLCollection);
 });
 
 test('DOM node should match markup', t => {
+	const { document, HTMLDivElement, HTMLSpanElement } = new Window();
+
 	const markup = `
   <div>
     <span>hello</span>
@@ -26,7 +30,7 @@ test('DOM node should match markup', t => {
   </div>
   `;
 	const expectedText = ['hello', 'world'];
-	const domNode = createNode(markup);
+	const domNode = createNode(markup, document);
 
 	t.true(domNode instanceof HTMLDivElement);
 	t.is(domNode.children.length, 2);
@@ -37,7 +41,9 @@ test('DOM node should match markup', t => {
 });
 
 test('DOM node should insert into DOM', t => {
-	const domNode = createNode('<div></div>');
+	const { document } = new Window();
+
+	const domNode = createNode('<div></div>', document);
 	document.body.appendChild(domNode);
 	t.is(document.body.lastChild, domNode);
 });
